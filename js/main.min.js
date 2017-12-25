44e55754232861d4f7b330be155b6becef9a16d2
GemmaBeferull <gemmabecala@gmail.com>
2017-12-25 23:24:55 +0100

    colors and grid library

--- js/main.min.js
+++ js/main.min.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
 			$('#fullpage').fullpage({
-				sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+				sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'red', '#ccddff'],
 				scrollingSpeed: 1000,
         navigation: true,
 				navigationPosition: 'right',
