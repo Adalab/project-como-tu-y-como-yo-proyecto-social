1f189547620c645a594c98de4dc275a892cb93da
“Ana” <amartinezaguilar6@gmail.com>
2017-12-25 21:47:10 +0100

    Instal library fullpage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('>> Ready :)');
+$(document).ready(function() {
+			$('#fullpage').fullpage({
+				sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+				scrollingSpeed: 1000,
+        navigation: true,
+				navigationPosition: 'right',
+				navigationTooltips: ['First page', 'Second page', 'Third and last page']
+			});
+		});
