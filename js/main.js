34502f4a77f694a2e2a95f65781460dbb0562e49
RocioBellon <rociobelloncezar@gmail.com>
2018-01-02 20:47:27 +0100

    arrow done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
 $(document).ready(function() {
-			$('#fullpage').fullpage({
-				sectionsColor: ['#fff', '#fff', '#fff', 'fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#261d1a'],
-				scrollingSpeed: 1000,
-        navigation: true,
-				navigationPosition: 'right',
-				navigationTooltips: ['First page', 'Second page', 'Third and last page']
-			});
-		});
+	$('#fullpage').fullpage({
+		sectionsColor: ['#fff', '#fff', '#fff', 'fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#261d1a'],
+		scrollingSpeed: 1000,
+		navigation: true,
+		navigationPosition: 'right',
+		navigationTooltips: ['First page', 'Second page', 'Third and last page']
+	});
+});
+
+//JS para la flecha
+
+var buttonScrollDown = document.querySelector('#button-scrolldown');
+function scrollDown(event){
+	$.fn.fullpage.moveSectionDown();
+	event.preventDefault();
+}
+
+buttonScrollDown.addEventListener('click', scrollDown);
