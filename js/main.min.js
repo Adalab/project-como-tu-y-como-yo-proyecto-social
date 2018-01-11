20c4e10a1553ed081f66a1af99411dfddc638f38
GemmaBeferull <gemmabecala@gmail.com>
2018-01-11 19:22:32 +0100

    transition section5

--- js/main.min.js
+++ js/main.min.js
@@ -1,15 +1,32 @@
 'use strict';
 
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-		sectionsColor: ['#fff', '#fff', '#fff', 'fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#261d1a'],
-		scrollingSpeed: 1000,
-		navigation: true,
-		navigationPosition: 'right',
-		navigationTooltips: ['First page', 'Second page', 'Third and last page']
-	});
-});
+var picSectionInfoPic = document.querySelector ('.info-img');
+var textSectionInfoPic = document.querySelector ('.text-sec-info-pic');
 
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#fff', '#fff', '#fff', 'fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#261d1a'],
+        scrollingSpeed: 1000,
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['First page', 'Second page', 'Third and last page'],
+
+
+
+        afterLoad: function(anchorLink, index){
+            var loadedSection = $(this);
+
+
+            if(index == 6){
+
+							picSectionInfoPic.classList.add('info-img-transition')
+							textSectionInfoPic.classList.add('info-img-transition')
+
+            }
+
+        }
+    });
+});
 //JS para la flecha
 
 var buttonScrollDown = document.querySelector('#button-scrolldown');
